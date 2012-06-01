31206a62537895a5b4b32d46ba2e4c8f6b6caebc
bobray <bobray@softville.com>
2012-05-31 22:29:32 -0500

    added notify snippet to package

--- _build/data/transport.snippets.php
+++ _build/data/transport.snippets.php
@@ -42,6 +42,16 @@ $snippets = array();
 $snippets[1]= $modx->newObject('modSnippet');
 $snippets[1]->fromArray(array(
     'id' => 1,
+    'name' => 'Notify',
+    'description' => 'Send site updates to Subscribers and Twitter',
+    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/notify.snippet.php'),
+),'',true,true);
+
+
+
+$snippets[2]= $modx->newObject('modSnippet');
+$snippets[2]->fromArray(array(
+    'id' => 2,
     'name' => 'RemoveNotify',
     'description' => 'Removes all parts of Notify package except the files in core/components/notify/  and this snippet',
     'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/removenotify.snippet.php'),
