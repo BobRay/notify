0b6ef438cd30dfb8184455613aeb793c2b15ebc7
bobray <bobray@softville.com>
2012-06-04 16:09:44 -0500

    added properties comment

--- core/components/notify/elements/snippets/notify.snippet.php
+++ core/components/notify/elements/snippets/notify.snippet.php
@@ -32,7 +32,80 @@
  *
  */
 
-/* [[+properties]] */
+/* Properties:
+ * @property &urlShorteningService list -- Service used to shorten all
+ *    URLs in text and Tweet; Default: none.
+ * @property &groups textfield -- Comma-separated list of User Groups
+ *    to send to (no spaces). The Subscribers group will be set in the form,
+ *    but if you delete it and submit with the Groups field empty, email
+ *    will be sent to all users on the site; Default: (empty)..
+ * @property &tags textfield -- (optional) Comma-separated list of
+ *    tags (no spaces). If set, only users in specified Groups with the
+ *    interest(s) set will receive the email; Default: (empty)..
+ * @property &notifyFacebook combo-boolean -- Notify Facebook via
+ *    Twitter with #fb in tweet -- must be set up in the Facebook Twitter
+ *    App; Default: (empty)..
+ * @property &twitterConsumerKey textfield -- Twitter Consumer Key;
+ *    Default: (empty)..
+ * @property &twitterConsumerSecret textfield -- Twitter Consumer
+ *    Secret; Default: (empty)..
+ * @property &twitterOauthToken textfield -- Twitter Access Token;
+ *    Default: (empty)..
+ * @property &twitterOauthSecret textfield -- Twitter Access Token
+ *    Secret; Default: (empty)..
+ * @property &bitlyApiKey textfield -- bit.ly API key (required);
+ *    Default: (empty)..
+ * @property &bitlyUsername textfield -- bit.ly username (required);
+ *    Default: (empty)..
+ * @property &googleApiKey textfield -- Google API key; Default:
+ *    (empty)..
+ * @property &suprApiKey textfield -- StumbleUpon API key (optional);
+ *    Default: (empty)..
+ * @property &suprUsername textfield -- Stumble Upon Username
+ *    (optional); Default: (empty)..
+ * @property &tinyurlApiKey textfield -- TinyUrl API key (optional);
+ *    Default: (empty)..
+ * @property &tinyurlUsername textfield -- TinyUrl username
+ *    (optional); Default: (empty)..
+ * @property &mailFrom textfield -- (optional) MAIL_FROM setting for
+ *    email; Default: emailsender System Setting.
+ * @property &mailFromName textfield -- (optional) MAIL_FROM_NAME
+ *    setting for email; Default: site_name System Setting.
+ * @property &mailSender textfield -- (optional) EMAIL_SENDER setting
+ *    for email; Default: emailsender System Setting.
+ * @property &mailReplyTo textfield -- (optional) REPLY_TO setting for
+ *    email; Default: emailsender System Setting.
+ * @property &nfFormTpl textfield -- Name of chunk to use for the
+ *    Notify form; Default: NfNotifyFormTpl.
+ * @property &nfEmailTpl textfield -- Name of chunk to use for the
+ *    Email to send to subscribers; Default: NfSubscriberEmailTpl.
+ * @property &nfTweetTpl textfield -- Name of chunk to use for the
+ *    Tweet text; Default: nfTweetTpl.
+ * @property &nfSubjectTpl textfield -- Name of chunk to use for the
+ *    Email subject; Default: NfEmailSubjectTpl.
+ * @property &nfTestEmailAddress textfield -- (optional) Email address
+ *    for test email; Default: emailsender System Setting.
+ * @property &sortBy textfield -- (optional) Field to sort by when
+ *    selecting users; Default: username.
+ * @property &userClass textfield -- (optional) class of the user
+ *    object. Only necessary if you have subclassed the user object;
+ *    Default: modUser.
+ * @property &sortByAlias textfield -- (optional) class of the user
+ *    object. Only necessary if you have subclassed the user object;
+ *    Default: modUser.
+ * @property &profileAlias textfield -- (optional) class of the user
+ *    profile object. Only necessary if you have subclassed the user profile
+ *    object; Default: modUserProfile.
+ * @property &profileClass textfield -- (optional) class of the user
+ *    profile object. Only necessary if you have subclassed the user profile
+ *    object; Default: modUser.
+ * @property &batchSize textfield -- (optional) Batch size for bulk
+ *    email to subscribers; Default: 50.
+ * @property &batchDelay textfield -- (optional) Delay between batches
+ *    in seconds; Default: 1.
+ * @property &itemDelay textfield -- (optional) Delay between
+ *    individual emails in seconds; Default: .51.
+ */
 
 /* @var $modx modX */
 /* @var $scriptProperties array */
