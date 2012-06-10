5bd196c561241730dd54df1fbf871027fa6eebdf
bobray <bobray@softville.com>
2012-06-10 17:41:01 -0500

    internationalized strings in Notify form

---
+++ core/components/notify/lexicon/en/form.inc.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Notify
+ *
+ * 
+ *
+ * Notify is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Notify is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Notify; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package notify
+ */
+/**
+ * form Lexicon Topic
+ *
+ * @package notify
+ * @subpackage lexicon
+ */
+
+/* Strings for use in the Notify form */
+
+$_lang['nf.notify_subscribers'] = 'Notify Subscribers';
+$_lang['nf.send_test_email'] = 'Send Test Email';
+$_lang['nf.email_address_for_test'] = 'Email address for Test';
+$_lang['nf.email_subject'] = 'Email Subject';
+$_lang['nf.groups'] = 'Groups (comma-separated list of groups to send to)';
+$_lang['nf.tags'] = 'Tags (comma-separated list of tags)';
+$_lang['nf.text_for_email'] = 'Text for Email';
+$_lang['nf.email_preview'] = 'Email Preview';
+$_lang['nf.notify_twitter'] = 'Notify Twitter';
+$_lang['nf.send_tweet'] = 'Send Tweet';
+$_lang['nf.text_for_tweet'] = 'Text for Tweet';
