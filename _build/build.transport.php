af511eec9d779e714a9191fbe750c4a78fa165cc
bobray <bobray@softville.com>
2013-01-31 00:44:14 -0500

    version to 1.1.1 -- released

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -32,7 +32,7 @@
 /* Set package info be sure to set all of these */
 define('PKG_NAME','Notify');
 define('PKG_NAME_LOWER','notify');
-define('PKG_VERSION','1.1.0');
+define('PKG_VERSION','1.1.1');
 define('PKG_RELEASE','pl');
 define('PKG_CATEGORY','Notify');
 
