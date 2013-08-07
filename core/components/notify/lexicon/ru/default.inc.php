393c713614442d55aad005eeeb4a0c181622a0d4
viktorminator@gmail.com <viktorminator@gmail.com>
2013-08-07 18:49:50 +0300

    Russian lexicon

---
+++ core/components/notify/lexicon/ru/default.inc.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Notify
+ *
+ * 
+ *
+ * Notify is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Notify is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Notify; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package notify
+ */
+/**
+ * Default Lexicon Topic
+ *
+ * @package notify
+ * @subpackage lexicon
+ */
+
+/* notify default strings */
+
+$_lang['notify'] = 'Notify';
+$_lang['nf.unauthorized_access'] = 'Неавторизированный доступ &dash; должен принадлежать к группе администраторов';
+$_lang['nf.could_not_find_preview_page'] = 'Страница NotifyPreview не найдена';
+$_lang['nf_page_id_is_empty'] = '$_POST pageId пуст';
+$_lang['nf.could_not_get_resource'] = 'Ресурс не получен';
+$_lang['nf.could_not_find_email_tpl_chunk'] = 'Не найден шаблонирующий чанк для email';
+$_lang['nf.could_not_find_tweet_tpl_chunk'] = 'Не найден шаблонирующий чанк для твиттера';
+$_lang['nf.could_not_find_user_group'] = 'Не найдена група пользователя';
+$_lang['nf.no_recipients_to_send_to'] = 'Нет получателей для отправки';
+$_lang['nf.bulk_emails_not_sent'] = 'Объёмная почта не отправляется';
+$_lang['nf.could_not_open_log_file'] = 'Не могу открыть файл логов (убедитесь, что директория логов существует)';
+$_lang['nf.no_profile_for'] = 'У пользователя нет профиля';
+$_lang['nf.sbs_extended_field_not_set'] = 'Параметр sbs_extended_field не установлен';
+$_lang['nf.has_no_email_address'] = 'Нет адреса email';
+$_lang['nf.test_email_address_empty'] = 'Адрес почты пуст';
+$_lang['nf.test_email_not_sent'] = 'Тестовое письмо не отправлено';
+$_lang['nf.mail_error_sending_test_email'] = 'Ошибка при отправке тестового письма';
+$_lang['nf.test_email_sent_successfully'] = 'Тестовое email отправлено успешно';
+$_lang['nf.twitter_consumer_key_not_set'] = 'Ключ пользователя Twitter не установлен';
+$_lang['nf.twitter_consumer_secret_not_set'] = 'Секретный ключ пользователя Twitter не установлен';
+$_lang['nf.twitter_access_token_not_set'] = 'Токен доступа Twitter не установлен';
+$_lang['nf.twitter_access_token_secret_not_set'] = 'Секретный токен доступа Twitter не установлен';
+$_lang['nf.tweet_field_is_empty'] = 'Поле твита не заполнено';
+$_lang['nf.unknown_error_using_twitter_api'] = 'При использовании Twitter API возникла неизвестная ошибка';
+$_lang['nf.twitter_said_there_was_an_error'] = 'Twitter уведомил о возникновении ошибки';
+$_lang['nf.full_response'] = 'Полный ответ: ';
+$_lang['nf.email_to_subscribers_sent_successfully'] = 'Email было успешно отправлено к Подписчикам в количестве [[+nf_number]] получателей';
+$_lang['nf.tweet_sent_successfully'] = 'Твит успешно отправлен';
---
+++ core/components/notify/lexicon/ru/properties.inc.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Notify
+ *
+ *
+ *
+ * Notify is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Notify is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Notify; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package notify
+ */
+/**
+ * Properties (property descriptions) Lexicon Topic
+ *
+ * @package notify
+ * @subpackage lexicon
+ */
+
+/* Notify Property Description strings */
+
+/* Lexicon Strings in: properties.notify.php */
+
+$_lang['nf.url_shortening_service_desc'] = 'Служба, которая используется для сокращения всех URL в тексте и твиттах.';
+$_lang['nf.groups_desc'] = 'Список через запятую груп пользователей к которым слать почту (без пробелов). Група Subscribers будет установлена в форме, но если вы удалите её и отправите с пустым полем для Груп, то почта будет отправлена ВСЕМ пользователям сайта';
+$_lang['nf.tags_desc'] = ' (опционально) Список тегов через запятую (без пробелов). Выставляется, только если пользователи в определённой групе указали интерес(ы) - темы, по которым хотят получать уведомления.';
+$_lang['nf.notify_facebook_desc'] = 'Уведомить Facebook через Twitter с помощью #fb в твитте -- должен быть настроен Facebook Twitter App.';
+$_lang['nf.twitter_consumer_key_desc'] = 'Ключ пользователя Twitter';
+$_lang['nf.twitter_consumer_secret_desc'] = 'Секретный ключ пользователя Twitter';
+$_lang['nf.twitter_access_token_desc'] = 'Токен доступа Twitter';
+$_lang['nf.twitter_oauth_token_secret_desc'] = 'Секретный ключ доступа Twitter';
+$_lang['nf.bitly_api_key_desc'] = 'bit.ly API ключ (необходим)';
+$_lang['nf.bitly_username_desc'] = 'bit.ly username (необходим)';
+$_lang['nf.google_api_key_desc'] = 'Google API ключ';
+$_lang['nf.supr_api_key_desc'] = 'StumbleUpon API ключ (опционально)';
+$_lang['nf.supr_username_desc'] = 'StumbleUpon имя пользователя (опционально)';
+$_lang['nf.tinyurl_api_key_desc'] = 'TinyUrl API key (опционально)';
+$_lang['nf.tinyurl_username_desc'] = 'TinyUrl username (опционально)';
+$_lang['nf.mail_from_desc'] = '(опционально) MAIL_FROM настройки для email. По-умолчанию: системная настройка emailsender';
+$_lang['nf.mail_from_name_desc'] = '(опционально) MAIL_FROM_NAME настройки для email. По-умолчанию: системная настройка site_name';
+$_lang['nf.mail_sender_desc'] = '(опционально) EMAIL_SENDER настройки для email. По-умолчанию: системная настройка emailsender';
+$_lang['nf.mail_reply_to_desc'] = '(опционально) REPLY_TO настройки для email. По-умолчанию: системная настройка emailsender';
+$_lang['nf.form_tpl_desc'] = 'Название чанка для использования в Notify форме; по-умолчанию: NfNotifyFormTpl';
+$_lang['nf.email_tpl_new_desc'] = 'Название чанка для использования для почты нового ресурса; по-умолчанию: NfSubscriberEmailTplNew';
+$_lang['nf.email_tpl_existing_desc'] = 'Название чанка для использования для почты обновлённого ресурса; по-умолчанию: NfSubscriberEmailTplExisting';
+$_lang['nf.email_tpl_custom_desc'] = 'Название чанка для использования для пользовательской почты; по-умолчанию: NfSubscriberEmailTplCustom';
+$_lang['nf.tweet_tpl_new_desc'] = 'Название чанка для использования для текста твиттера нового ресурса; по-умолчанию: nfTweetTplNew';
+$_lang['nf.tweet_tpl_existing_desc'] = 'Название чанка для использования для текста твиттера обновлённого ресурса; по-умолчанию: nfTweetTplExisting';
+$_lang['nf.tweet_tpl_custom_desc'] = 'Название чанка для использования в пользовательском тексте для твиттера; по-умолчанию: nfTweetTplCustom';
+$_lang['nf.subject_tpl_desc'] = 'Название чанка для использования в теме письма; по-умолчанию: NfEmailSubjectTpl';
+$_lang['nf.test_email_address_desc'] = ' (опционально) Email адрес для тестового письма. По-умолчанию: системная настройка emailsender';
+$_lang['nf.sortby_desc'] = ' (опционально) Поля для сортировки при выборе пользователей; по-умолчанию: username';
+$_lang['nf.userClass_desc'] = ' (опционально) класс объекта пользователь. Необходим только если вы расширили вас объект пользователя. По-умолчанию: modUser';
+$_lang['nf.sortby_alias_desc'] = ' (опционально) класс объекта пользователь. Необходим только если вы расширили вас объект пользователя. По-умолчанию: modUser';
+$_lang['nf.profile_desc'] = ' (опционально) class of the user profile object. Необходим только если вы расширили вас объект профиль пользователя. По-умолчанию: modUserProfile';
+$_lang['nf.profile_class_desc'] = ' (опционально) class of the user profile object. Необходим только если вы расширили вас объект профиль пользователя. По-умолчанию: modUser';
+$_lang['nf.batch_size_desc'] = ' (опционально) Объем партии для массовой рассылки подписчикам. По-умолчанию: 50';
+$_lang['nf.batch_delay_desc'] = ' (опционально) Задержка между партиями в секундах. По-умолчанию: 1';
+$_lang['nf.item_delay_desc'] = ' (опционально) Задержка между отдельными письмами в секундах. По-умолчанию: .51';
+$_lang['nf.pref_list_chunk_name_desc'] = ' (опционально) Чанк для вывода списка предпочтений (тегов). По-умолчанию: sbsPrefListTpl';
+$_lang['nf.require_all_tags_default_desc'] = ' (опционально) устанавливает значение по-умолчанию для чекбокса Require All Tags; если выставлено, то только те пользователи будут получать письма, которые выставили все теги; по-умолчанию: No';
+$_lang['nf.unsubscribe_tpl_desc'] = 'Название чанка для ссылки по отмене подписки.';
