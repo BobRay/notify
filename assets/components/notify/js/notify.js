5cdd1ed928c34eca8b74c5c99133879ad0e27e6a
bobray <bobray@softville.com>
2014-03-07 18:17:06 -0500

    Scroll to bottom; set up for multiple processors

--- assets/components/notify/js/notify.js
+++ assets/components/notify/js/notify.js
@@ -3,138 +3,182 @@
    Placeholders below are set in the snippet properties
 */
 
-/*$(document).ready(function () {
-    start();
-});*/
 
 $(document).ready(function (event) {
     $('#pb_button').click(function () {
 
-    /* If no action selected, submit (reload) form */
-    if (($('#nf_notify').prop('checked') == false)
-            && ($('#nf_send_test_email').prop('checked') == false)
-            && ($('#nf_send_tweet').prop('checked') == false)) {
-       $('#nf_form').submit();
-        return true;
-    }
+        /* If no action selected, submit (reload) form */
+        if (($('#nf_notify').prop('checked') == false)
+                && ($('#nf_send_test_email').prop('checked') == false)
+                && ($('#nf_send_tweet').prop('checked') == false)) {
+           $('#nf_form').submit();
+            return true;
+        }
 
-    $('#nf_results').empty();
-    $('#nf_results').hide();
+        $('#nf_results').find('span').remove();
+        $('#nf_results').find('br').remove();
+        $('#nf_results').hide();
 
-    /* One or more actions selected. Run Processor */
-    /* start the notify-process snippet, ignore the return value
-     * this needs to be at the top so the process snippet
-     * can write to the file and this ajax call can complete
-     * before the second ajax call tries to read the file
-     */
+        /** ToDo: make this generic */
+        var connectorUrl = "http://localhost/addons/assets/components/notify/connector.php";
 
-    var connectorUrl = "http://localhost/addons/assets/components/notify/connector.php";
-    $.ajax({
-        type: "POST",
-        data: {
-            'action': 'mgr/nfsend',
-            'send_bulk' : $("#nf_notify").prop('checked') == true,
-            'single_id': $("#nf_test_email_address").val(),
-            'email_subject': $("#nf_email_subject").val(),
-            'email_text': $("#nf_email_text").val(),
-            'groups': $("#nf_groups").val(),
-            'tags': $("#nf_tags").val(),
-            'require_all_tags': $("#nf_require_all_tags").prop('checked') == true,
-            'single': $("#nf_send_test_email").prop('checked') == true,
-            'send_tweet': $("#nf_send_tweet").prop('checked') == true,
-            'tweet_text': $("#nf_tweet_text").val()
-        },
-        dataType: "json",
-        cache: false,
-        url: connectorUrl,
-        success: function(data) {
-           data['errors'].forEach(function (err, i) {
-                console.log("Error: " + err);
-               $('<br /><span class="nf_error">' + err + '</span>').appendTo("#nf_results")
+
+        /* One or more actions selected */
+
+        /* IF send_tweet is checked, call sendTweet processor */
+        if ($("#nf_send_tweet").prop('checked') == true) {
+            $.ajax({
+                type: "POST",
+                data: {
+                    'action': 'mgr/nfsendtweet',
+                    'tweet_text': $("#nf_tweet_text").val()
+                },
+                dataType: "json",
+                cache: false,
+                url: connectorUrl,
+                success: function (data) {
+                   data['errors'].forEach(function (err, i) {
+                       console.log("Error: " + err);
+                       $('<span class="nf_error">' + err + '</span><br />').appendTo("#nf_results")
+
+
+                   });
+                   data['successMessages'].forEach(function (msg, i) {
+                       console.log("Success: " + msg);
+                       $('<span class="nf_success">' + msg + '</span><br />').appendTo("#nf_results")
+                   });
+                   $("#nf_results").slideDown("slow");
+                    var $target = $('html,body');
+                    $target.animate({scrollTop: $target.height()}, 1000);
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                   alert("Status: " + textStatus);
+                   alert("Error: " + errorThrown);
+                }
 
 
             });
-           data['successMessages'].forEach(function (msg, i) {
-               console.log("Success: " + msg);
-               $('<br /><span class="nf_success">' + msg + '</span>').appendTo("#nf_results")
-           });
-            $("#results").slideDown("slow");
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-           alert("Status: " + textStatus);
-           alert("Error: " + errorThrown);
         }
-    });
 
-    if  ($("#nf_notify").prop('checked') == true) {
-        var url = "http://localhost/addons/notify-status.html";
+        /* start the notify-process snippet, ignore the return value
+         * this needs to be at the top so the process snippet
+         * can write to the file and this ajax call can complete
+         * before the second ajax call tries to read the file
+         */
+        if ($("#nf_notify").prop('checked') == true) {
 
-        $("#pb_progressbar").progressbar({
-            value: 0,
-            max: 100
-        });
+            $.ajax({
+                type: "POST",
+                data: {
+                    'action': 'mgr/nfsendbulk',
+                    'send_bulk' : $("#nf_notify").prop('checked') == true,
+                    'single_id': $("#nf_test_email_address").val(),
+                    'email_subject': $("#nf_email_subject").val(),
+                    'email_text': $("#nf_email_text").val(),
+                    'groups': $("#nf_groups").val(),
+                    'tags': $("#nf_tags").val(),
+                    'require_all_tags': $("#nf_require_all_tags").prop('checked') == true,
+                    'single': $("#nf_send_test_email").prop('checked') == true,
+                    'send_tweet': $("#nf_send_tweet").prop('checked') == true,
+                    'tweet_text': $("#nf_tweet_text").val()
+                },
+                dataType: "json",
+                cache: false,
+                url: connectorUrl,
+                success: function(data) {
+                   data['errors'].forEach(function (err, i) {
+                        console.log("Error: " + err);
+                       $('<span class="nf_error">' + err + '</span><br />').appendTo("#nf_results")
 
-        $("#pb_button").hide();
-        $("#pb_progressOuter").slideDown("slow");
 
+                    });
 
+                   data['successMessages'].forEach(function (msg, i) {
+                       console.log("Success: " + msg);
+                       $('<span class="nf_success">' + msg + '</span><br />').appendTo("#nf_results")
+                   });
 
-
-        /* set the timer that checks the status.php file for progress reports */
-        var timer = setInterval(function(){
-        $.ajax({
-            type: "POST",
-            url: url,
-            cache: false,
-            data: {},
-            dataType:"json",
-            //crossDomain: true,
-
-            /* update the progress bar and text messages if the file changes */
-            success: function(data){
-                if (data.percent >= 100) {
-                    clearInterval(timer);
-                    $("#pb_progressOuter").hide("slow");
-                    $("#pb_button").show();
-                    /* Clear for future runs */
-                    data.percent = 0;
-                    data.text1="";
-                    data.text2="";
-
+                   $("#nf_results").slideDown("slow");
+                    var $target = $('html,body');
+                    $target.animate({scrollTop: $target.height()}, 1000);
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                   alert("Status: " + textStatus);
+                   alert("Error: " + errorThrown);
                 }
-                $("#pb_progressbar").progressbar("value",data.percent);
-                $("#pb_percent").text(data.percent);
-                $("#pb_text2").text(data.text2);
-                $("#pb_text1").text(data.text1);
+            });
 
-            },
-        error : function (x, d, e) {
-              if (x.status == -5) {
-                  alert("You are offline!! Please Check Your Network.");
-              } else {
-                  if (x.status == 404) {
-                      alert("Requested URL not found");
+
+            var url = "http://localhost/addons/notify-status.html";
+
+            $("#pb_progressbar").progressbar({
+                value: 0,
+                max: 100
+            });
+
+            $("#pb_button").hide();
+            $("#pb_progressOuter").slideDown("slow");
+            var $target = $('html,body');
+            $target.animate({scrollTop: $target.height()}, 1000);
+
+
+
+
+            /* set the timer that checks the status.php file for progress reports */
+            var timer = setInterval(function(){
+            $.ajax({
+                type: "POST",
+                url: url,
+                cache: false,
+                data: {},
+                dataType:"json",
+                //crossDomain: true,
+
+                /* update the progress bar and text messages if the file changes */
+                success: function(data){
+                    if (data.percent >= 100) {
+                        clearInterval(timer);
+                        $("#pb_progressOuter").hide("slow");
+                        $("#pb_button").show();
+                        /* Clear for future runs */
+                        data.percent = 0;
+                        data.text1="";
+                        data.text2="";
+
+                    }
+                    $("#pb_progressbar").progressbar("value",data.percent);
+                    $("#pb_percent").text(data.percent);
+                    $("#pb_text2").text(data.text2);
+                    $("#pb_text1").text(data.text1);
+
+                },
+            error : function (x, d, e) {
+                  if (x.status == -5) {
+                      alert("You are offline!! Please Check Your Network.");
                   } else {
-                      if (x.status == 500) {
-                          alert("Internal Server Error.");
+                      if (x.status == 404) {
+                          alert("Requested URL not found");
                       } else {
-                          if (e == "parsererror") {
-                              alert("Error: Parsing JSON Request failed.");
+                          if (x.status == 500) {
+                              alert("Internal Server Error.");
                           } else {
-                              if (e == "timeout") {
-                                  alert("Request Time out.");
+                              if (e == "parsererror") {
+                                  alert("Error: Parsing JSON Request failed.");
                               } else {
-                                  alert("Unknown Error: " + x.responseText);
+                                  if (e == "timeout") {
+                                      alert("Request Time out.");
+                                  } else {
+                                      alert("Unknown Error: " + x.responseText);
+                                  }
                               }
                           }
                       }
                   }
               }
-          }
-       });
-       },800);
-    }
-    return false;
-    })
+           });
+           },800);
+        }
+        return false;
+        })
 
 });
---
+++ core/components/notify/processors/mgr/nfsendtweet.class.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * Class getTaskStatsProcessor
+ */
+class NfSendProcessor extends modProcessor {
+
+    protected $props;
+    protected $errors;
+    protected $successMessages;
+    protected $testMode;
+
+
+    public function initialize() {
+        $config = $this->modx->fromJSON($_SESSION['nf_config']);
+        $this->properties = array_merge($this->properties, $config);
+        $this->testMode = $this->getProperty('testMode',false);
+        $this->setCheckbox('send_tweet');
+        $this->setCheckbox('send_bulk');
+        $this->setCheckbox('require_all_tags');
+        $this->setCheckbox('single');
+
+        return true;
+    }
+    /**
+     * @param $percent int
+     * @param $text1 string
+     * @param $text2 string
+     * @param $pb_target modChunk
+     */
+    public  function update($percent, $text1, $text2, &$pb_target) {
+
+
+        $msg = $this->modx->toJSON(array(
+            'percent' => $percent,
+            'text1'   => $text1,
+            'text2'   => $text2,
+        ));
+
+            /*return $this->modx->toJSON(array(
+                'success' => true,
+                'stats'   => $stats
+            ));*/
+
+        /* use a chunk for the status "file" */
+
+        $pb_target->setContent($msg);
+        $pb_target->save();
+    }
+
+    public function checkPermissions() {
+        $valid =  $this->modx->hasPermission('view_user');
+        if (! $valid) {
+            $this->setError($this->modx->lexicon('nf.no_view_user_permission~~User does not have view_user permission'));
+        }
+        return $valid;
+    }
+
+    public function process($scriptProperties = array()) {
+
+
+        $chunk = $this->modx->getObject('modChunk', array('name' => 'Debug'));
+
+        if (isset($this->properties)) {
+            $content =  print_r($this->properties, true);
+        } else {
+            $content = 'No Props';
+        }
+        $chunk->setContent($content);
+        $chunk->save();
+
+        $sendBulk = (bool) $this->getProperty('send_bulk', false);
+
+        if ($sendBulk) {
+            $this->sendBulk();
+        }
+
+        /* sleep(3); */
+
+
+        $sendTweet = (bool)  $this->getProperty('send_tweet', false);
+
+        if ($sendTweet) {
+           $this->send_tweet();
+        }
+
+        $this->errors[] = 'Dummy Error msg 1';
+        $this->errors[] = 'Dummy Error msg 2';
+        $this->successMessages[] = 'Dummy Success msg 1';
+        $this->successMessages[] = 'Dummy Success msg 2';
+
+        $results["status"] = empty($this->errors)? "Yes" : "No";
+        $results["errors"] = $this->errors;
+        $results["successMessages"] = $this ->successMessages;
+        return $this->modx->toJSON($results);
+    }
+
+    protected function sendBulk() {
+        $statusChunk = $this->modx->getObject('modChunk', array('name' => 'NfStatus'));
+
+        $msg1 = $this->getProperty('groups', 'No Groups');
+        $msg2 = $this->getProperty('tags', 'No Tags');
+        for ($i = 1; $i <= 120; $i++) {
+
+            $this->update($i, $msg1, $msg2, $statusChunk);
+            set_time_limit(0);
+            usleep(100000);
+        }
+        $this->update(0, 'Starting', '', $statusChunk);
+
+
+    }
+    protected function send_single() {
+
+    }
+    protected function send_tweet() {
+
+        require_once(MODX_CORE_PATH . 'components/notify/model/notify/twitteroauth.php');
+        $consumer_key = $this->getProperty('twitterConsumerKey', NULL);
+        if (!$consumer_key) {
+            $this->setError($this->modx->lexicon('nf.twitter_consumer_key_not_set'));
+        }
+        $consumer_secret = $this->getProperty('twitterConsumerSecret', NULL);
+        if (!$consumer_secret) {
+            $this->setError($this->modx->lexicon('nf.twitter_consumer_secret_not_set'));
+        }
+        $oauth_token = $this->getProperty('twitterOauthToken', NULL);
+        if (!$oauth_token) {
+            $this->setError($this->modx->lexicon('nf.twitter_access_token_not_set'));
+        }
+        $oauth_secret = $this->getProperty('twitterOauthSecret', NULL);
+        if (!$oauth_secret) {
+            $this->setError($this->modx->lexicon('nf.twitter_access_token_secret_not_set'));
+        }
+        $msg = $this->getProperty('tweet_text', '');
+        /*$msg = $this->tweetText;*/
+        if (empty($msg)) {
+            $this->setError($this->modx->lexicon('nf.tweet_field_is_empty'));
+        } else {
+            $tweet = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token, $oauth_secret);
+            $response = $this->testMode
+                ? array()
+                : $tweet->post('statuses/update', array('status' => $msg));
+            /* This will get recent tweets */
+            /* $response = $tweet->get('statuses/user_timeline', array('screen_name' => 'BobRay')); */
+
+            if ($response === NULL) {
+                $this->setError($this->modx->lexicon('nf.unknown_error_using_twitter_api'));
+            } elseif (isset($response->errors)) {
+                $this->setError('<p>' . $this->modx->lexicon('nf.twitter_said_there_was_an_error') .
+                    ': ' . $response->errors[0]->message . '</p><br />');
+            } else {
+                $this->setSuccess($this->modx->lexicon('nf.tweet_sent_successfully'));
+            }
+        }
+
+    }
+
+    public function setError($msg) {
+        $this->errors[] = $msg;
+    }
+
+    public function setSuccess($msg) {
+        $this->successMessages[] = $msg;
+    }
+}
+
+return 'NfSendProcessor';
