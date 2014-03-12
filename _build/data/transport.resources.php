fb06add1a33149192035d30f043319af26d33418
bobray <bobray@softville.com>
2014-03-12 18:18:24 -0400

    Add Notify Status resource to build

--- _build/data/transport.resources.php
+++ _build/data/transport.resources.php
@@ -101,4 +101,42 @@ $resources[2]->fromArray(array (
 ), '', true, true);
 $resources[2]->setContent(file_get_contents($sources['data'].'resources/notifypreview.content.html'));
 
+$resources[3] = $modx->newObject('modResource');
+$resources[3]->fromArray(array (
+  'id' => 3,
+  'type' => 'document',
+  'contentType' => 'text/html',
+  'pagetitle' => 'NotifyStatus',
+  'longtitle' => '',
+  'description' => '',
+  'alias' => 'notify-status',
+  'link_attributes' => '',
+  'published' => true,
+  'isfolder' => false,
+  'introtext' => '',
+  'richtext' => false,
+  'template' => 0,
+  'menuindex' => 68,
+  'searchable' => false,
+  'cacheable' => false,
+  'createdby' => 1,
+  'editedby' => 1,
+  'deleted' => false,
+  'deletedon' => 0,
+  'deletedby' => 0,
+  'menutitle' => '',
+  'donthit' => false,
+  'privateweb' => false,
+  'privatemgr' => false,
+  'content_dispo' => 0,
+  'hidemenu' => true,
+  'class_key' => 'modDocument',
+  'context_key' => 'web',
+  'content_type' => 1,
+  'hide_children_in_tree' => 0,
+  'show_in_tree' => 1,
+  'properties' => NULL,
+), '', true, true);
+$resources[3]->setContent(file_get_contents($sources['data'].'resources/notifystatus.content.html'));
+
 return $resources;
